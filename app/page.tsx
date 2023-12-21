c37454ab6ef8bb025d2dbb7ec5ff392da2530fe7
_Enoabasi_Essien_ <enoabasidream@gmail.com>
2023-12-21 18:35:11 +0400

    refac: adding the brand styles for the app.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import Link from 'next/link';
+import { buttonVariants } from '@/components/ui/button';
 
 export default async function Home() {
   return (
     <main className='flex flex-col items-center justify-between min-h-screen p-24'>
       <div className='space-y-4'>
-        <h1>HiConvos</h1>
-        <Link href={'/auth/sign-in'}>Get started</Link>
+        <h1 className='text-2xl font-bold md:text-4xl'>Welcome to HiConvos</h1>
+        <Link
+          href={'/auth/sign-in'}
+          className={buttonVariants({ variant: 'brand' })}
+        >
+          Get started 👍
+        </Link>
       </div>
     </main>
   );
